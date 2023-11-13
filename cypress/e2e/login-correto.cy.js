7a9ba8cea6d9a74e7785d0a04bb7d422f6f03c2a
carlossh8 <carlos.sh8@outlook.com>
2023-11-13 20:32:51 -0300

    Ajuste login

--- cypress/e2e/login-correto.cy.js
+++ cypress/e2e/login-correto.cy.js
@@ -3,7 +3,7 @@
 describe('Página de login', () => {
     it('Preencher o login corretamente', () => {
       cy.visit('http://localhost:4200/#/home')
-      cy.get('data-test="loginUserName"]').type('Fulano1')
+      cy.get('data-test="loginUserName"]').type('fulano1')
       cy.get('data-test="loginPassword"]').type('fulano123')
       cy.contains('button','login').click();
     })
