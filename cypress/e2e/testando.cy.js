e661161c8410a2d38c9a40bec0ac5f0caf3eb9be
carlossh8 <carlos.sh8@outlook.com>
2023-11-13 18:56:40 -0300

    Aula 07 - Ajustando a seleção

--- cypress/e2e/testando.cy.js
+++ cypress/e2e/testando.cy.js
@@ -1,11 +1,11 @@
-describe('template spec', () => {
-  it('passes', () => {
+describe('Página de cadastro', () => {
+  it('Preencher os campos de registrar agora', () => {
     cy.visit('http://localhost:4200/#/home')
     cy.contains('a','Register now').click();
-    cy.get('[formcontrolname="email"]').type('testecy@uorak.com')
-    cy.get('[formcontrolname="fullName"]').type('fulano de Tal')
-    cy.get('[formcontrolname="userName"]').type('fulano1')
-    cy.get('[formcontrolname="password"]').type('fulano123')
+    cy.get('[data-test="email"]').type('testecy@uorak.com')
+    cy.get('[data-test="fullName"]').type('fulano de Tal')
+    cy.get('[data-test="registerUserName"]').type('fulano2')
+    cy.get('[data-test="registerPassword"]').type('fulano1234')
     cy.contains('button','Register').click();
   })
 })
