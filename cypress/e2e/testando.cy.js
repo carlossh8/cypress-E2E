d9d05d3f2858ae60a363e894249104234e03af18
carlossh8 <carlos.sh8@outlook.com>
2023-11-13 18:38:28 -0300

    Aula 06 - Criando o primeiro teste

--- cypress/e2e/testando.cy.js
+++ cypress/e2e/testando.cy.js
@@ -1,5 +1,11 @@
 describe('template spec', () => {
   it('passes', () => {
-    cy.visit('https://www.google.com.br/?hl=pt-BR')
+    cy.visit('http://localhost:4200/#/home')
+    cy.contains('a','Register now').click();
+    cy.get('[formcontrolname="email"]').type('testecy@uorak.com')
+    cy.get('[formcontrolname="fullName"]').type('fulano de Tal')
+    cy.get('[formcontrolname="userName"]').type('fulano1')
+    cy.get('[formcontrolname="password"]').type('fulano123')
+    cy.contains('button','Register').click();
   })
 })
