2e382e6bef0ea6e0c1f4c26f6078232706df4156
carlossh8 <carlos.sh8@outlook.com>
2023-11-13 19:27:26 -0300

    Add cenário negativo

---
+++ cypress/e2e/cadastro-incorreto.cy.js
@@ -0,0 +1,14 @@
+/// <reference types="Cypress" />
+
+describe('Página de cadastro', () => {
+    it('Preencher os campos de "registrar agora" de forma incorreta', () => {
+      cy.visit('http://localhost:4200/#/home')
+      cy.contains('a','Register now').click();
+      cy.contains('button','Register').click();
+      cy.contains('button','Register').click();
+      cy.contains('Email is required!').should('be.visible');
+      cy.contains('Full name is required!').should('be.visible');
+      cy.contains('User name is required!').should('be.visible');
+      cy.contains('Password is required!').should('be.visible');
+    })
+  })
