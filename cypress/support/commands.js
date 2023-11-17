1d603256138e6b72264ff44b658bceed6a1534a4
carlossh8 <carlos.sh8@outlook.com>
2023-11-17 17:48:04 -0300

    Update Files

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -3,5 +3,5 @@ Cypress.Commands.add('login', (nome, senha) => {
     cy.get('[data-test="loginUserName"]').type(nome);
     cy.get('[data-test="loginPassword"]').type(senha);
     cy.get('[data-test="loginBtn"]').click();
-    cy.contains('a', '(Logout)').should('be.visible');
+    cy.contains('a','(Logout)').should('be.visible');
 })
